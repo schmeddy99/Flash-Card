56b026af80b4f9c884c7522546d7ce9fd7b6c9dd
schmeddy99 <ahmedaosman00@gmail.com>
2025-05-13 14:05:35 -0700

    Adding background color, padding around card, and the flashcard canvas

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ root = Tk()
 root.title("Flashy")
 root.configure(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0)
+
 
 root.mainloop()
